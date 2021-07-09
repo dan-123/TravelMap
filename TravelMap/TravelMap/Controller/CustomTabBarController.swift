2b1f07ebaf5a843a6f8ae922dfe07db4d8a41b29
dan-123 <mgn.petrov@gmail.com>
2021-07-09 15:59:19 +0500

    Custom tabBar
    Небольшие работы с таб баром

--- TravelMap/TravelMap/Controller/CustomTabBarController.swift
+++ TravelMap/TravelMap/Controller/CustomTabBarController.swift
@@ -11,58 +11,12 @@ class CustomTabBarController: UITabBarController {
     
     // MARK: - Properties
     
-    private let mapButtonDiameter: CGFloat = 75
-    
-//    private let redColor: UIColor = UIColor(red: 254.0 / 255.0, green: 116.0 / 255.0, blue: 96.0 / 255.0, alpha: 1.0)
-//    private let greenColor: UIColor = UIColor(red: 102.0 / 255.0, green: 166.0 / 255.0, blue: 54.0 / 255.0, alpha: 1.0)
+    private lazy var customTabBar: CustomTabBarView = {
+       let customTabBar = CustomTabBarView()
+        customTabBar.translatesAutoresizingMaskIntoConstraints = false
+        return customTabBar
+    }()
 
-    private lazy var mapButton: UIButton = {
-        let button = UIButton()
-        button.layer.cornerRadius = mapButtonDiameter / 2
-        button.backgroundColor = .systemBlue
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-    
-    private lazy var mapImageView: UIImageView = {
-        let image = UIImageView()
-        image.image = UIImage(named: "map")
-        image.contentMode = .scaleToFill
-        image.tintColor = .white
-        image.translatesAutoresizingMaskIntoConstraints = false
-        return image
-    }()
-    
-    private lazy var placesButton: UIButton = {
-        let button = UIButton()
-//        button.layer.cornerRadius = mapButtonDiameter /2 
-        button.backgroundColor = .systemGray
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-    
-    private lazy var placesImageView: UIImageView = {
-        let image = UIImageView()
-        image.image = UIImage(named: "places")
-        image.contentMode = .scaleToFill
-        image.tintColor = .white
-        image.translatesAutoresizingMaskIntoConstraints = false
-        return image
-    }()
-    
-    // MARK: - Init
-    
-//    init() {
-//        super.init(nibName: nil, bundle: nil)
-//        tabBar = CustomTabBar()
-//    }
-//
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-    
-    
-    
     // MARK: - Lyfe cycle
     
     override func viewDidLoad() {
@@ -76,49 +30,26 @@ class CustomTabBarController: UITabBarController {
     // MARK: - UI
     
     private func setupElements() {
-        tabBar.addSubview(mapButton)
-        mapButton.addSubview(mapImageView)
-        
-        tabBar.addSubview(placesButton)
-        placesButton.addSubview(placesImageView)
+        tabBar.addSubview(customTabBar)
     }
     
     private func setupConstraint() {
-        //map
-        NSLayoutConstraint.activate([
-            mapButton.heightAnchor.constraint(equalToConstant: mapButtonDiameter),
-            mapButton.widthAnchor.constraint(equalToConstant: mapButtonDiameter),
-            mapButton.centerXAnchor.constraint(equalTo: tabBar.centerXAnchor),
-//            mapButton.topAnchor.constraint(equalTo: tabBar.topAnchor, constant: -20),
-            mapButton.bottomAnchor.constraint(equalTo: tabBar.bottomAnchor, constant: -10),
-            
-            mapImageView.heightAnchor.constraint(equalToConstant: 45),
-            mapImageView.widthAnchor.constraint(equalToConstant: 45),
-            mapImageView.centerXAnchor.constraint(equalTo: mapButton.centerXAnchor),
-            mapImageView.centerYAnchor.constraint(equalTo: mapButton.centerYAnchor)
-        ])
         
-        //places
         NSLayoutConstraint.activate([
-            placesButton.heightAnchor.constraint(equalToConstant: mapButtonDiameter-25),
-            placesButton.widthAnchor.constraint(equalToConstant: mapButtonDiameter-25),
-            placesButton.leadingAnchor.constraint(equalTo: tabBar.leadingAnchor, constant: 24),
-            placesButton.topAnchor.constraint(equalTo: tabBar.topAnchor, constant: -20),
-
-            placesImageView.heightAnchor.constraint(equalToConstant: 25),
-            placesImageView.widthAnchor.constraint(equalToConstant: 25),
-            placesImageView.centerXAnchor.constraint(equalTo: placesButton.centerXAnchor),
-            placesImageView.centerYAnchor.constraint(equalTo: placesButton.centerYAnchor)
+            customTabBar.leadingAnchor.constraint(equalTo: tabBar.leadingAnchor, constant: 0),
+            customTabBar.trailingAnchor.constraint(equalTo: tabBar.trailingAnchor, constant: 0),
+            customTabBar.topAnchor.constraint(equalTo: tabBar.topAnchor, constant: 0),
+            customTabBar.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0)
         ])
     }
     
     private func setupControllers() {
+        
         let pointViewController = UINavigationController.init(rootViewController: PlacesViewController())
+//        pointViewController.tabBarItem.image = 
         let mapViewConroller = UINavigationController.init(rootViewController: MapViewController())
         let thirdViewController = UINavigationController.init(rootViewController: ThirdViewController())
-        
-        
-        
+
         viewControllers = [pointViewController, mapViewConroller, thirdViewController]
         selectedIndex = 1
         
---
+++ TravelMap/TravelMap/View/TabBarView/CustomTabBarView.swift
@@ -0,0 +1,93 @@
+//
+//  CustomTabBarView.swift
+//  TravelMap
+//
+//  Created by Даниил Петров on 09.07.2021.
+//
+
+import UIKit
+
+class CustomTabBarView: UIView {
+    
+    // MARK: - Properties
+    
+    private let placesButtonDiameter: CGFloat = 20
+    private let mapButtonDiameter: CGFloat = 75
+
+    private lazy var mapButton: UIButton = {
+        let button = UIButton()
+        button.layer.cornerRadius = mapButtonDiameter / 2
+        button.backgroundColor = .systemBlue
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var mapImageView: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "map")
+        image.contentMode = .scaleToFill
+        image.tintColor = .white
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private lazy var placesImageView: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "places")
+        image.contentMode = .scaleToFill
+        image.tintColor = .white
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupElement()
+        setupConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI
+    
+    private func setupElement() {
+        addSubview(mapButton)
+        mapButton.addSubview(mapImageView)
+        
+        addSubview(placesImageView)
+    }
+    
+    private func setupConstraint() {
+        NSLayoutConstraint.activate([
+            mapButton.heightAnchor.constraint(equalToConstant: mapButtonDiameter),
+            mapButton.widthAnchor.constraint(equalToConstant: mapButtonDiameter),
+            mapButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+//            mapButton.topAnchor.constraint(equalTo: tabBar.topAnchor, constant: -20),
+            mapButton.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5),
+            
+            mapImageView.heightAnchor.constraint(equalToConstant: 45),
+            mapImageView.widthAnchor.constraint(equalToConstant: 45),
+            mapImageView.centerXAnchor.constraint(equalTo: mapButton.centerXAnchor),
+            mapImageView.centerYAnchor.constraint(equalTo: mapButton.centerYAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            placesImageView.heightAnchor.constraint(equalToConstant: placesButtonDiameter),
+            placesImageView.widthAnchor.constraint(equalToConstant: placesButtonDiameter),
+            //            mapButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            placesImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            placesImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 50)
+            //            mapButton.topAnchor.constraint(equalTo: tabBar.topAnchor, constant: -20),
+        ])
+
+    }
+    
+    // MARK: - Methods
+}
+
+// MARK: - Extensions
