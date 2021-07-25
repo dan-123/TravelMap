354caa71b54d9188b21ac0d9ebf0686394cd4d75
dan-123 <mgn.petrov@gmail.com>
2021-07-25 21:18:27 +0500

    Setting view controller
    Добавил scroll view на экране с информацией о приложении.

--- TravelMap/TravelMap/Controller/InformationViewController.swift
+++ TravelMap/TravelMap/Controller/InformationViewController.swift
@@ -18,6 +18,14 @@ class InformationViewController: UIViewController {
         return informationView
     }()
     
+    lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.scrollsToTop = true
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .green
+        return scrollView
+    }()
+    
     private let informationModel = InformationModel()
     
     // MARK: - Lyfe cycle
@@ -34,15 +42,32 @@ class InformationViewController: UIViewController {
     // MARK: - UI
     
     func setupElements() {
-        view.addSubview(informationView)
+//        view.addSubview(informationView)
+        view.addSubview(scrollView)
+        scrollView.addSubview(informationView)
     }
     
     func setupConstraint() {
+//        NSLayoutConstraint.activate([
+//            informationView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+//            informationView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+//            informationView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+//            informationView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 8)
+//        ])
+        
         NSLayoutConstraint.activate([
-            informationView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
-            informationView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
-            informationView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
-            informationView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 8)
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -8)
         ])
+        
+        NSLayoutConstraint.activate([
+            informationView.topAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.topAnchor, constant: 15),
+            informationView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 15),
+            informationView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -15),
+            informationView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -15)
+        ])
+        
     }
 }
